a12e36f4163b49b3dd3406a89b2d63f4cab6556f
j-mecham <josh.mecham@gmail.com>
2023-04-21 14:28:23 -0600

    Changed to fulfill task 1.2. Added array of pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello World');
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+    {name: 'Pikachu', height: 4, types: ['field','fairy']},
+    {name: 'Luxray', height: 14, types: ['field']},
+    {name: 'Ninetales', height: 11, types: ['field']}
+];
