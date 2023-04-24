38d420c8338f8eab3ed1334a09e39fb090c08829
j-mecham <josh.mecham@gmail.com>
2023-04-24 11:53:03 -0600

    Added for loop that prints the name and height of the pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,20 @@ let pokemonList = [
     {name: 'Luxray', height: 14, types: ['field']},
     {name: 'Ninetales', height: 11, types: ['field']}
 ];
+
+/*
+    This loop iterates over the array 
+    and writes to the document the name and 
+    height of the pokemon.
+    It also checks the height value and sends a message
+    if the pokemon is taller than 11
+*/
+for (let i = 0; i < pokemonList.length; i++) {
+    let name = pokemonList[i].name;
+    let height = pokemonList[i].height;
+    document.write(name + ' (height: ' + height + ')');
+    if (height > 11) {
+        document.write(' - Wow, that\'s big!')
+    }
+    document.write('<br><br>');
+}
