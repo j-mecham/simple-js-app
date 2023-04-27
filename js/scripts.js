5c7fcaa2d1a8c04c67aa1dcb23fe908bf3749252
j-mecham <josh.mecham@gmail.com>
2023-04-27 11:43:46 -0600

    Updated so the pokemonlist is in an IIFE, replaced for with forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,39 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-    {name: 'Pikachu', height: 4, types: ['field','fairy']},
-    {name: 'Luxray', height: 14, types: ['field']},
-    {name: 'Ninetales', height: 11, types: ['field']}
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur', 
+            height: 7, 
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Pikachu', 
+            height: 4, 
+            types: ['field','fairy']
+        },
+        {
+            name: 'Luxray', 
+            height: 14, 
+            types: ['field']
+        },
+        {
+            name: 'Ninetales', 
+            height: 11, 
+            types: ['field']
+        }
+    ]
+    function getAll () {
+        return pokemonList;
+    }
+    function add (pokemon) {
+        pokemonList.push(pokemon);
+    }
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
+
+console.log(pokemonRepository.getAll())
 
 /*
     This loop iterates over the array 
@@ -12,7 +42,7 @@ let pokemonList = [
     It also checks the height value and sends a message
     if the pokemon is taller than 11
 */
-for (let i = 0; i < pokemonList.length; i++) {
+/*for (let i = 0; i < pokemonList.length; i++) {
     let name = pokemonList[i].name;
     let height = pokemonList[i].height;
     document.write(name + ' (height: ' + height + ')');
@@ -21,3 +51,17 @@ for (let i = 0; i < pokemonList.length; i++) {
     }
     document.write('<br><br>');
 }
+*/
+
+function getPokeInfo(poke) {
+    let name = poke.name;
+    let height = poke.height;
+    document.write(name + ' (height: ' + height + ')');
+    if (height > 11) {
+        document.write(' - Wow, that\'s big!')
+    }
+    document.write('<br><br>')
+}
+
+pokemonRepository.getAll().forEach(getPokeInfo)
+
